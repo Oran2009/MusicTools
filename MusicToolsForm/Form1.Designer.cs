9ae61a020ed6fad697ca973f92be72d14881f9fc
Ben Hoang <banhquanhoang@gmail.com>
2020-05-26 16:22:34 -0700

    Add project files.

---
+++ MusicToolsForm/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace MusicToolsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import Audio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Import Video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(182, 62);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Combine Audio and Video";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1077, 349);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(426, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(651, 98);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(425, 154);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(652, 40);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(425, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(652, 40);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(227, 343);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1104, 373);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "MusicTools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MusicToolsForm/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using NAudio;
+using WaveFormRendererLib;
+
+namespace MusicToolsForm
+{
+    public partial class Form1 : Form
+    {
+        List<Panel> listPanel = new List<Panel>();
+        List<string> videos = new List<string>();
+        string single_audio, single_video, save_location;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listPanel.Add(panel1);
+            listPanel[0].BringToFront();
+            textBox2.Text = "Imported Audio: ";
+            textBox3.Text = "Imported Video: ";
+        }
+
+        //SINGLE-IMPORT AUDIO
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //GET AUDIO FILE
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Audio Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "wav",
+                Filter = "wav files (*.wav)|*.wav",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                single_audio = openFileDialog1.FileName;
+                textBox1.Text += "Opened Audio File Successfully" + "\r\n";
+                textBox2.Text = "Imported Audio: " + single_audio;
+            }
+
+            if (single_audio != null)
+            {
+                //DISPLAY WAVEFORM
+                var maxPeakProvider = new MaxPeakProvider();
+                var rmsPeakProvider = new RmsPeakProvider(200); // e.g. 200
+                var samplingPeakProvider = new SamplingPeakProvider(200); // e.g. 200
+                var averagePeakProvider = new AveragePeakProvider(4); // e.g. 4
+
+                var myRendererSettings = new StandardWaveFormRendererSettings();
+                myRendererSettings.Width = 651;
+                myRendererSettings.TopHeight = 24;
+                myRendererSettings.BottomHeight = 24;
+
+                var renderer = new WaveFormRenderer();
+                var audioFilePath = single_audio;
+                var image = renderer.Render(audioFilePath, averagePeakProvider, myRendererSettings);
+
+                pictureBox1.Image = image;
+            }
+            
+        }
+
+        //SINGLE-IMPORT VIDEO
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //GET VIDEO FILE
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Video Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "mp4",
+                Filter = "mp4 files (*.mp4)|*.mp4",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                single_video = openFileDialog1.FileName;
+                textBox1.Text += "Opened Video File Successfully" + "\r\n";
+                textBox3.Text = "Imported Video: " + single_video;
+            }
+
+        }
+
+        //SINGLE-SYNC
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //SINGLE-COMBINE
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Lets user choose folder to save the combined video to
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            folderDlg.Description = "Select a folder to save the combined video to:";
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                save_location = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+
+            //Checks if video and audio file is imported, then combines using the following FFMPEG batch script
+            //ffmpeg -i VIDEOFILE.mp4 -i AUDIOFILE.wav -map 0:v -map 1:a -c:v copy -c:a aac -b:a 160k RESULT.mp4
+            string errormsg = "";
+            if (single_video == null)
+            {
+                errormsg += "Missing video file";
+            }
+            if (single_audio == null)
+            {
+                errormsg += "\r\n" + "Missing audio file";
+            }
+            if (save_location == null)
+            {
+                errormsg += "\r\n" + "Invalid save location";
+            }
+            if (errormsg != "")
+            {
+                MessageBox.Show(errormsg);
+            }
+            else
+            {
+                Process combine = new Process();
+                combine.StartInfo.FileName = "ffmpeg";
+                combine.StartInfo.UseShellExecute = false;
+                combine.StartInfo.Arguments = "-i " + single_video + " -i " + single_audio + " -map 0:v -map 1:a -c:v copy -c:a aac -b:a 160k " + save_location + @"\combined.mp4";
+                combine.StartInfo.RedirectStandardOutput = true;
+                combine.Start();
+                textBox1.Text += "Saved to " + save_location + " as combined.mp4";               
+            }
+            save_location = null;
+        }
+
+        //EXTRA BUTTONS
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
